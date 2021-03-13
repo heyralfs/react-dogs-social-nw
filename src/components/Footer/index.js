b1624bdf94161d1f4aeb29873db6fe484648e4fe
Ralf <ralfoliveira@gmail.com>
2021-03-13 20:04:48 -0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,18 @@
-//import React from "react";
-//import styles from "./style.module.css";
+import React from "react";
+import styles from "./style.module.css";
+import { ReactComponent as Dogs } from "../../assets/dogs-footer.svg";
 
 export default function Footer() {
-  return <div></div>;
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados.</p>
+      <p>
+        Orgulhosamente desenvolvido no curso React Completo da{" "}
+        <a href="https://origamid.com" target="_blank">
+          Origamid
+        </a>
+      </p>
+    </footer>
+  );
 }
